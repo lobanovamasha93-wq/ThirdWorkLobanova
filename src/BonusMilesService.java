f21abfd31f57aa39821a93e6d1392e6be6802be9
Maria Lobanova <marusa.lobanova2015@yandex.ru>
2026-01-19 15:34:26 +0300

    Fix service

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,7 +1,7 @@
 public class BonusMilesService {
 
     public int calculate(int price) {
-        final int RUBLES_PER_MILE = 20;  // Константа: рублей за 1 милю
-        return price / RUBLES_PER_MILE;
+        final int RublesPerMile = 20;
+        return price / RublesPerMile;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,11 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // Создаём объект сервиса для расчёта миль
+
         BonusMilesService service = new BonusMilesService();
 
-        // Задаём стоимость билета (можно менять для проверки)
-        int price = 15_444;
+        int price = 13_745;
 
-        // Вызываем метод расчёта и получаем количество миль
         int miles = service.calculate(price);
 
         // Выводим результат на экран
